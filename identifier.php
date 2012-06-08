166e37b57e6e87537b759dba3a1d51442739e34d
Mike Fortner <michaeljamesfortner@gmail.com>
2012-06-08 12:59:58 -0700

    identifier.php put your info into it but leave mine commented out

--- identifier.php
+++ identifier.php
@@ -6,6 +6,20 @@ $dbuser = 'fortnerm-db';
 $dbpass = 'KubP1OidG5uYDuBX';
 
 $server_salt = 0xceb1606c ; //7626b3f3607924b284b25a71dd2ec49a9fe5059052713715a0a3fb3a
+<<<<<<< HEAD
+=======
+
+
+//Mike's settings
+/*$homepage = "http://web.engr.oregonstate.edu/~fortnerm/CS-275-Project/";
+$dbhost = 'oniddb.cws.oregonstate.edu';
+$dbname = 'fortnerm-db';
+$dbuser = 'fortnerm-db';
+$dbpass = 'KubP1OidG5uYDuBX';
+
+$server_salt = 0xceb1606c ; //7626b3f3607924b284b25a71dd2ec49a9fe5059052713715a0a3fb3a
+*/
+>>>>>>> identifier.php put your info into it but leave mine commented out
 ?>
 
 
